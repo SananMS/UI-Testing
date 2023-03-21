02351dd2c3ea8492f86e7f77971b8bac6cdf99d7
Sanan <mammadovsanan98@gmail.com>
2023-03-21 13:42:48 +0200

    General refactor: Adding new try and catch blocks

--- src/test/java/org/example/UITest.java
+++ src/test/java/org/example/UITest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -32,25 +31,43 @@ public class UITest {
     @Test
     public void isThePositionShownOnThePage() {
         // Open the web browser at the PlayTech Url
-        driver.get("https://www.playtech.ee");
+        try {
+            driver.get("https://www.playtech.ee");
+        } catch (Exception e) {
+            // Handle the exception if url is not available at the moment
+            Assertions.fail("Failed to open PlayTech website: " + e.getMessage());
+        }
 
         // Find internship tab coordinates
-        WebElement element = driver.findElement(By.xpath("//a[@href='/internship']"));
-        Point point = element.getLocation();
+        WebElement element = null;
+        try {
+            element = driver.findElement(By.xpath("//a[@href='/internship']"));
+        } catch (Exception e) {
+            // Handle the exception if the internship tab is not found
+            Assertions.fail("Failed to find Internship tab: " + e.getMessage());
+        }
 
         // X coordinate
-        int xCord = point.getX();
+        int xCoordinate = element.getLocation().getX();
 
         // Y coordinate
-        int yCord = point.getY();
+        int yCoordinate = element.getLocation().getY();
 
+        // Clicking on the Internship tab
         Actions action = new Actions(driver);
-        // Increasing the value of xCord for being able to click on Internship instead of Careers tab
-        action.moveByOffset(xCord + 10, yCord).click().perform();
 
+        // Increasing the value of xCoordinate for being able to click on Internship instead of Careers tab
+        action.moveByOffset(xCoordinate + 10, yCoordinate).click().perform();
 
         // Check if "Development QA Engineer (Intern)" position is shown on the page
-        WebElement position = driver.findElement(By.xpath("//*[contains(text(),'Development QA Engineer (Intern)')]"));
+        WebElement position = null;
+        try {
+            position = driver.findElement(By.xpath("//*[contains(text(),'Development QA Engineer (Intern)')]"));
+        } catch (Exception e) {
+            // Handle the exception if the position is not found
+            Assertions.fail("Failed to find Development QA Engineer (Intern) position: " + e.getMessage());
+        }
+
         isPositionShownOnThePage = position.isDisplayed();
         Assertions.assertTrue(isPositionShownOnThePage);
     }
