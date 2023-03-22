c40576d3cf8c2067657424858c758888de1e1f20
Sanan <mammadovsanan98@gmail.com>
2023-03-22 23:39:55 +0200

    General refactor: Addition of methods to UITest class; 1 extra test case

---
+++ src/main/java/org/example/Page.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class Page {
+    private final WebDriver driver;
+    private final ChromeOptions options;
+    private final Actions action;
+    private boolean isStatusOK;
+
+    public Page() {
+        this.isStatusOK = true;
+        this.options = new ChromeOptions ();
+        this.options.addArguments("--start-maximized");
+        this.driver = new ChromeDriver(options);
+        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        this.action = new Actions(this.driver);
+    }
+
+    public void setIsStatusOK(boolean isStatusOK) {
+        this.isStatusOK = isStatusOK;
+    }
+
+    public boolean getIsStatusOK() {
+        return this.isStatusOK;
+    }
+
+    public void writePositionStatusToFile(boolean isPositionShownOnThePage, String fileName) throws IOException {
+        File file = new File(fileName);
+        FileWriter writer = new FileWriter(file);
+        writer.write("Position Status: " + (isPositionShownOnThePage ? "available" : "unavailable"));
+        writer.close();
+    }
+
+    public void goToPage (String pageName) {
+        try {
+            this.driver.get(pageName);
+        }
+        catch (Exception e) {
+            this.setIsStatusOK(false);
+        }
+
+    }
+
+    public WebElement lookForElement(String elementName) {
+        try {
+            return this.driver.findElement(By.xpath("//*[contains(normalize-space(text())," + "'" + elementName + "'" + ")]"));
+        } catch (Exception e) {
+            this.setIsStatusOK(false);
+            return null;
+        }
+    }
+
+    public void clickByCoordinate(WebElement element) {
+        try {
+            int xCoordinate = element.getLocation().getX();
+            int yCoordinate = element.getLocation().getY();
+            // Increasing the value of xCoordinate for being able to click on Internship instead of Careers tab
+            action.moveByOffset(xCoordinate + 10, yCoordinate).click().perform();
+        } catch (Exception e) {
+            this.setIsStatusOK(false);
+        }
+    }
+
+    public void quit() {
+        this.driver.quit();
+    }
+}
--- src/test/java/org/example/UITest.java
+++ src/test/java/org/example/UITest.java
@@ -1,85 +1,44 @@
 package org.example;
 
-import org.junit.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class UITest {
-
-    private WebDriver driver;
-    private boolean isPositionShownOnThePage;
+    Page page;
 
     @BeforeEach
     public void initialize() {
-        ChromeOptions options = new ChromeOptions ();
-        options.addArguments("--start-maximized");
-        driver = new ChromeDriver(options);
+        page = new Page();
     }
 
     @Test
-    public void isThePositionShownOnThePage() {
-        // Open the web browser at the PlayTech Url
-        try {
-            driver.get("https://www.playtech.ee");
-        } catch (Exception e) {
-            // Handle the exception if url is not available at the moment
-            Assertions.fail("Failed to open PlayTech website: " + e.getMessage());
-        }
+    public void isThePositionShownOnThePageByCoordinate() throws IOException {
+        page.goToPage("https://www.playtech.ee");
+        WebElement element = page.lookForElement("Internship");
+        page.clickByCoordinate(element);
+        page.lookForElement("Development QA Engineer (Intern)");
+        boolean isPositionShown = page.getIsStatusOK();
+        page.writePositionStatusToFile(isPositionShown, "position_check_by_coordinate.txt");
+        Assertions.assertTrue(isPositionShown, "Position is not shown on the page");
+    }
 
-        // Find internship tab element
-        WebElement element = null;
-        try {
-            element = driver.findElement(By.xpath("//a[@href='/internship']"));
-        } catch (Exception e) {
-            // Handle the exception if the internship tab is not found
-            Assertions.fail("Failed to find Internship tab: " + e.getMessage());
-        }
-
-        // X coordinate
-        int xCoordinate = element.getLocation().getX();
-
-        // Y coordinate
-        int yCoordinate = element.getLocation().getY();
-
-        Actions action = new Actions(driver);
-        // Increasing the value of xCoordinate for being able to click on Internship instead of Careers tab
-        action.moveByOffset(xCoordinate + 10, yCoordinate).click().perform();
-
-        // Check if "Development QA Engineer (Intern)" position is shown on the page
-        WebElement position = null;
-        try {
-            position = driver.findElement(By.xpath("//*[contains(normalize-space(text()),'Development QA Engineer (Intern)')]"));
-        } catch (Exception e) {
-            // Handle the exception if the position is not found
-            Assertions.fail("Failed to find Development QA Engineer (Intern) position: " + e.getMessage());
-        }
-
-        isPositionShownOnThePage = position.isDisplayed();
-        Assertions.assertTrue(isPositionShownOnThePage);
+    @Test
+    public void isThePositionShownOnThePageByElement() throws IOException {
+        page.goToPage("https://www.playtech.ee");
+        WebElement element = page.lookForElement("Internship");
+        element.click();
+        page.lookForElement("Development QA Engineer (Intern)");
+        boolean isPositionShown = page.getIsStatusOK();
+        page.writePositionStatusToFile(isPositionShown, "position_check_by_element.txt");
+        Assertions.assertTrue(isPositionShown, "Position is not shown on the page");
     }
 
     @AfterEach
-    public void writePositionStatusToFile() throws IOException {
-        File file = new File("position_check.txt");
-        FileWriter writer = new FileWriter(file);
-        writer.write("Position Status: " + (isPositionShownOnThePage ? "available" : "unavailable"));
-        writer.close();
-    }
-
-    @After
     public void tearDown() {
-        driver.quit();
+        page.quit();
     }
 }
